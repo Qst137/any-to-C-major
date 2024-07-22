baba399088b92900b045e8ca1593dcab2e56c69e
kpt <2817151932@qq.com>
2024-07-23 00:13:53 +0800

    nothing changed; happy birthday

--- any-to-C-major/src/tests.rs
+++ any-to-C-major/src/tests.rs
@@ -20,10 +20,10 @@ use crate::{flag::SheetStyle, key::Key, parser::Parser, transformer::Transformer
 
 #[test]
 fn test_sheet_to_c() {
-    let sheet_string = "3212343,321#5673,3#23(211)776365".to_string();
-    println!("{sheet_string}");
+    let sheet_string = "1312312[55] [3]1[67]1[67]55 443234321[7] 11[7]1232 [77]21"
+        .to_string();
     let style = SheetStyle::Brackets;
-    let sheet = Parser::new(style, sheet_string).parse(Key::from_letter("f#"));
+    let sheet = Parser::new(style, sheet_string).parse(Key::from_letter("E"));
     let c_sheet = Transformer::new(sheet).to_c();
     let formatted = c_sheet.format(SheetStyle::Brackets);
     println!("{formatted}");
